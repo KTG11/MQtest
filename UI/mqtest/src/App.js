c97d88324651dcf8c856001cf7444cdd1eaa6f03
KTG11 <ktgsrilanka@gmail.com>
2025-10-18 02:00:51 +0530

    test 1 on app.js

--- UI/mqtest/src/App.js
+++ UI/mqtest/src/App.js
@@ -1,16 +1,12 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './login';
-import Login from './signup';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/AuthPage" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<signup />} />
-        {/* Add other linked pages here (when you add the links it can be redirected) here */}
-      </Routes>
+      <Switch>
+        <Route path="/auth" component={AuthPage} />
+      </Switch>
     </Router>
   );
 }
