eccc20fe8787ca7af55f8dec37f149b3e6546487
KTG11 <ktgsrilanka@gmail.com>
2025-10-18 02:42:17 +0530

    test 2 on app.js

--- UI/mqtest/src/App.js
+++ UI/mqtest/src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/auth" component={AuthPage} />
-      </Switch>
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
     </Router>
   );
 }
