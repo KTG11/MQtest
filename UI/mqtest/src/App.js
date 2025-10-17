97c8c435442287f7ebaa2e6de82c354b82238d71
KTG11 <ktgsrilanka@gmail.com>
2025-10-18 01:23:46 +0530

    AuthPage.jsx added

--- UI/mqtest/src/App.js
+++ UI/mqtest/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './login';
+import Login from './signup';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/AuthPage" />} />
         <Route path="/login" element={<Login />} />
-        {/* Add other routes here */}
+        <Route path="/signup" element={<signup />} />
+        {/* Add other linked pages here (when you add the links it can be redirected) here */}
       </Routes>
     </Router>
   );
