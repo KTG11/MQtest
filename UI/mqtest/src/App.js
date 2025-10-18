c5089321e8b614352664b395630ff02bb989d900
KTG11 <ktgsrilanka@gmail.com>
2025-10-18 19:36:36 +0530

    fixing redirect to auth.js

--- UI/mqtest/src/App.js
+++ UI/mqtest/src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './login';
-import Login from './signup';
-import Login from './AuthPage';
+import Login from './auth';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<signup />} />
-        {/* Add other linked pages here (when you add the links it can be redirected) here */}
+        <Route path="/" element={<Navigate to="/auth" />} />
+        <Route path="/auth" element={<auth />} />
+        {/* Add other routes here */}
       </Routes>
     </Router>
   );
